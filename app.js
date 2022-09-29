bcaf0607fce21ba208eaca3d5f1aa0efc8fbd70b
ima17 <ima.sudaraka@outlook.com>
2022-09-29 16:44:50 +0530

    Connect database

--- app.js
+++ app.js
@@ -3,7 +3,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const _ = require('lodash');
+const _ = require("lodash");
+const mongoose = require("mongoose");
 
 let posts = [];
 const homeStartingContent =
@@ -20,10 +21,17 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", function (req, res) {
- 
-  res.render("home", { startingContent: homeStartingContent,posts:posts });
+mongoose.connect("mongodb://localhost:27017/blogDB");
 
+const postSchema = {
+  title: String,
+  content: String,
+};
+
+const Post = mongoose.model("Post", postSchema);
+
+app.get("/", function (req, res) {
+  res.render("home", { startingContent: homeStartingContent, posts: posts });
 });
 
 app.get("/contact", function (req, res) {
@@ -31,35 +39,38 @@ app.get("/contact", function (req, res) {
 });
 
 app.get("/about", function (req, res) {
-  res.render("about", { aboutContent: aboutContent});
+  res.render("about", { aboutContent: aboutContent });
 });
 
 app.get("/compose", function (req, res) {
   res.render("compose");
 });
 
-app.get("/posts/:postName",function(req,res){
-  requiredName =  _.lowerCase(req.params.postName);
+app.get("/posts/:postName", function (req, res) {
+  requiredName = _.lowerCase(req.params.postName);
 
-
-  posts.forEach(function(post){
+  posts.forEach(function (post) {
     storedName = _.lowerCase(post.title);
 
-    if(storedName=== requiredName){
+    if (storedName === requiredName) {
       console.log("Match Found");
-      res.render("post", { title: post.title ,content :post.content });
+      res.render("post", { title: post.title, content: post.content });
     }
   });
-  
-  
 });
 
 app.post("/compose", function (req, res) {
-
   const post = {
-    "title" : req.body.postTitle,
-    "content"  : req.body.postBody,
-  }
+    title: req.body.postTitle,
+    content: req.body.postBody,
+  };
+
+  const newPost = new Post({
+    title: req.body.postTitle,
+    content: req.body.postBody,
+  });
+
+  newPost.save();
 
   posts.push(post);
 
@@ -69,4 +80,3 @@ app.post("/compose", function (req, res) {
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
-
